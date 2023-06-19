d8dd13419e93e57e89c0f66f541bfcfd3a96af93
Ivan <Viduck27@gmail.com>
2023-06-19 18:58:35 +0300

    init

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            //this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "_";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(820, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "_";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 497);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 84);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 497);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 84);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(784, 497);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 84);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(344, 49);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 21);
+            this.lblMsg.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            //this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button4.Location = new System.Drawing.Point(1039, 497);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(198, 54);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "АВТОРЕШЕНИЕ";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 776);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblMsg;
+        //private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        int n =3; 
+        List<int> C1 = new List<int>(); 
+        List<int> C2 = new List<int>(); 
+        List<int> C3 = new List<int>(); 
+        int activeRing;
+        int compMove = 0;
+        List<int> HanoiList = new List<int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Restart();
+
+        }
+
+        public void AddRing(List<int> LTo)
+        {
+            if (LTo.Count == 0)
+            {
+                LTo.Add(activeRing);
+                activeRing = 0;
+                lblMsg.Text = "";
+            }
+            else
+            {
+                if (LTo[LTo.Count - 1] > activeRing)
+                {
+                    LTo.Add(activeRing);
+                    activeRing = 0;
+                    lblMsg.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("нельзя");
+                }
+            }           
+        } 
+
+        public void TakeRing(List<int> L)
+        {
+            activeRing = L[L.Count-1];
+            L.RemoveAt(L.Count-1);
+            lblMsg.Text = "Вы сняли кольцо " + activeRing;
+        } 
+
+        public void Draw() 
+        {
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            for (int i = C1.Count-1; i>=0;i--)
+            { 
+                label1.Text = label1.Text + C1[i];
+            }
+            for (int i = C2.Count - 1; i >= 0; i--)
+            {
+                label2.Text = label2.Text + C2[i];
+            }
+            for (int i = C3.Count - 1; i >= 0; i--)
+            {
+                label3.Text = label3.Text + C3[i];
+            }
+            if (activeRing == 0)
+            {
+                BtnTextChange("снять");
+            }
+            else
+            {
+                BtnTextChange("надеть");
+            }
+
+            label1.Refresh(); //Добавил чтобы обнавлялось значение башен
+            label2.Refresh();
+            label3.Refresh();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (activeRing == 0)
+            {
+                TakeRing(C1);
+                Draw();
+            }
+            else
+            {
+                AddRing(C1);
+                Draw();
+            }
+            CheckWin(C2);
+            CheckWin(C3);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (activeRing == 0)
+            {
+                TakeRing(C2);
+                Draw();
+            }
+            else
+            {
+                AddRing(C2);
+                Draw();
+            }
+            CheckWin(C2);
+            CheckWin(C3);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (activeRing == 0)
+            {
+                TakeRing(C3);
+                Draw();
+            }
+            else
+            {
+                AddRing(C3);
+                Draw();
+            }
+            CheckWin(C2);
+            CheckWin(C3);
+        }
+
+        public void CheckWin(List<int> L)
+        {
+            bool f=false; //true - победа
+            if (L.Count == n)
+            { 
+                f=true;
+                for(int i = 0;i>=n-1;i++)
+                { 
+                    if (L[i+1] > L[i])
+                    {
+                        f = false;
+                        break;
+                    }
+                }
+            }
+            if (f == true)
+            {
+                Win();
+            }
+        } 
+
+        private void Win()
+        {
+            MessageBox.Show("Победа! Повторим?");
+            Restart();
+        } 
+
+        public void Restart()
+        {
+            compMove = 0;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            C1.Clear();
+            C2.Clear();
+            C3.Clear();
+            HanoiList.Clear();
+            for (int i = n; i > 0; i--)
+            {
+                C1.Add(i);
+            }
+            Draw();
+        } 
+
+        public void BtnTextChange(string c)
+        {
+            button1.Text = c.ToString();
+            button2.Text = c.ToString();
+            button3.Text = c.ToString();
+        }
+        public void Comp(int n, int i, int k)
+        {
+            if (n == 1)
+            {
+                HanoiList.Add(i);
+                HanoiList.Add(k);
+            }
+            else
+            {
+                int tmp = 6 - i - k;
+                Comp(n - 1, i, tmp);
+                HanoiList.Add(i);
+                HanoiList.Add(k);
+                Comp(n - 1, tmp, k);
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(200); // таймаут но по другому
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+           
+            if (compMove == 0)
+            {
+                Comp(n, 1, 2);
+                compMove = 1;
+                button4_Click(sender, e); // вызывается тут а не по таймеру
+            }
+            else
+            {
+                switch (HanoiList[compMove - 1])
+                {
+                    case 1:
+                        button1_Click(sender, e);
+                        break;
+                    case 2:
+                        button2_Click(sender, e);
+                        break;
+                    case 3:
+                        button3_Click(sender, e);
+                        break;
+                }
+               
+                compMove++;
+                button4_Click(sender, e);  // вызывается тут а не по таймеру
+            }
+        }
+    }
+}
